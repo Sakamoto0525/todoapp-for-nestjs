0546d10299419b160038ef9f88e06bb158926f0a
Sakamoto0525 <525masaya@gmail.com>
2024-04-25 02:27:29 +0900

    board 一覧取得テスト追加

--- src/module/domain/board/boards.controller.spec.ts
+++ src/module/domain/board/boards.controller.spec.ts
@@ -1,27 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoardController } from './boards.controller';
 import { BoardService } from './boards.service';
+import { PrismaService } from '../../prisma/prisma.service';
 
 describe('BoardsController', () => {
-  let appController: BoardController;
+  let boardController: BoardController;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
-    const boards: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [],
       controllers: [BoardController],
-      providers: [BoardService],
+      providers: [BoardService, PrismaService],
     }).compile();
 
-    appController = boards.get<BoardController>(BoardController);
+    boardController = module.get<BoardController>(BoardController);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
+      const aprilFirst = new Date('2021-04-01T00:00:00');
+      const want = {
+        id: 10,
+        title: 'title',
+        description: 'content',
+        createdAt: aprilFirst,
+        updatedAt: aprilFirst,
+      };
+      prismaService.board.findMany = jest.fn().mockImplementation(() => want);
+
       const dto = {
         title: 'title',
-        content: 'content',
+        description: 'description',
       };
 
-      expect(appController.findMany(dto)).toBe('Hello World!');
+      const re = boardController.findMany(dto);
+
+      expect(re['id']).toBe(want.id);
+      expect(re['title']).toBe(want.title);
+      expect(re['description']).toBe(want.description);
+      expect(re['createdAt']).toBe(want.createdAt);
+      expect(re['updatedAt']).toBe(want.updatedAt);
+      // then((result: Board[]) => {
+      //   expect(result).toEqual({
+      //     id: 10,
+      //     title: 'title',
+      //     description: 'content',
+      //     createdAt: aprilFirst,
+      //     updatedAt: aprilFirst,
+      //   });
+      // });
     });
   });
 });
