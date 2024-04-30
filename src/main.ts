1cc90812cb4112ee9ac354344a3a3118f624573a
Sakamoto0525 <525masaya@gmail.com>
2024-04-30 22:23:46 +0900

    cors対応（とりあえずは*）

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   const config = new DocumentBuilder()
     .setTitle('Todo App Example')
     .setDescription('The todo app API description')
